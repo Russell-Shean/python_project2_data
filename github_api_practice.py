43bf93f9b3a0fe179b3756fc71225e941647b337
Russ <russshean@gmail.com>
2023-05-30 22:02:19 -0400

    dictionaries from API

--- github_api_practice.py
+++ github_api_practice.py
@@ -5,6 +5,7 @@ import requests
 # define the API call
 url = 'https://api.github.com/search/repositories?q=language:R&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
+
 r = requests.get(url, headers = headers)
 
 print(f"Status code: {r.status_code}")
@@ -14,3 +15,27 @@ response_dictionary = r.json()
 
 # Print the dictionary keys from the returned results
 print(response_dictionary.keys())
+
+
+# Start looking at the dictionary of results
+print(f"Total number of repositories: {response_dictionary['total_count']}")
+
+
+# find sub dictionaries
+repo_dictionaries = response_dictionary["items"]
+
+# number of repos returned
+print(f"Number of repos returned: {len(repo_dictionaries)}")
+
+
+# look at the first repository
+repo_0 = repo_dictionaries[0]
+
+print(f"Number of items: {len(repo_0)}")
+
+for key in sorted(repo_0.keys()):
+	print(key)
+
+
+for i in range(len(repo_dictionaries)):
+	print(f"{repo_dictionaries[i]['name']}: {repo_dictionaries[i]['description']}")
